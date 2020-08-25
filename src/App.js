e0b91447e690d6c04210de70f776d196ed7fa5cd
Nick Suckow <suckow12@gmail.com>
2020-08-25 10:19:32 -0500

    Changed name to Dinner Me

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App-header">
-        <h1>RecipeMe</h1>
+        <h1>Dinner Me</h1>
       <div className="Body">
         <h4>Hit the button to randomly select a new recipe</h4>
         <RecipeSelector
