2e7d9d0129166a3eaeb2e49ecf450e9fafec982c
Nick Suckow <suckow12@gmail.com>
2020-08-25 12:03:03 -0500

    Update precache-manifest.db72687eb0cd48270e64caabd4e2431a.js

--- docs/precache-manifest.db72687eb0cd48270e64caabd4e2431a.js
+++ docs/precache-manifest.db72687eb0cd48270e64caabd4e2431a.js
@@ -1,30 +1,30 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "b8e4405e5bf700a8255fe37945b41f5b",
-    "url": "/DinnerMe/index.html"
+    "url": "/RecipeMe/index.html"
   },
   {
     "revision": "f3d6c3c1ba0f5406d33a",
-    "url": "/DinnerMe/static/css/main.1f6a0c0a.chunk.css"
+    "url": "/RecipeMe/static/css/main.1f6a0c0a.chunk.css"
   },
   {
     "revision": "5e1cf3f29f470533503e",
-    "url": "/DinnerMe/static/js/2.752fc1dc.chunk.js"
+    "url": "/RecipeMe/static/js/2.752fc1dc.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/DinnerMe/static/js/2.752fc1dc.chunk.js.LICENSE.txt"
+    "url": "/RecipeMe/static/js/2.752fc1dc.chunk.js.LICENSE.txt"
   },
   {
     "revision": "f3d6c3c1ba0f5406d33a",
-    "url": "/DinnerMe/static/js/main.0d8f47be.chunk.js"
+    "url": "/RecipeMe/static/js/main.0d8f47be.chunk.js"
   },
   {
     "revision": "6dd8ea1bf3dbc0397cd6",
-    "url": "/DinnerMe/static/js/runtime-main.4a6075b7.js"
+    "url": "/RecipeMe/static/js/runtime-main.4a6075b7.js"
   },
   {
     "revision": "de7b0a6e4dde3f0cd446c1ca48c2974e",
-    "url": "/DinnerMe/static/media/background.de7b0a6e.png"
+    "url": "/RecipeMe/static/media/background.de7b0a6e.png"
   }
 ]);
