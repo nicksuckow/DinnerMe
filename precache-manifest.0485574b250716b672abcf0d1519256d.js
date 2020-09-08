1bf7280de009153702d95358742c345b2dd698d2
Nick Suckow <suckow12@gmail.com>
2020-09-07 21:30:08 -0500

    Updates

---
+++ precache-manifest.0485574b250716b672abcf0d1519256d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e83067495967eb56b43d9be9e4c16f2c",
+    "url": "/RecipeMe/index.html"
+  },
+  {
+    "revision": "b86071b9643354bba052",
+    "url": "/RecipeMe/static/css/main.36a0a87f.chunk.css"
+  },
+  {
+    "revision": "5e1cf3f29f470533503e",
+    "url": "/RecipeMe/static/js/2.752fc1dc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/RecipeMe/static/js/2.752fc1dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b86071b9643354bba052",
+    "url": "/RecipeMe/static/js/main.5bd1ac00.chunk.js"
+  },
+  {
+    "revision": "872842f326097ead26e0",
+    "url": "/RecipeMe/static/js/runtime-main.52acd908.js"
+  },
+  {
+    "revision": "de7b0a6e4dde3f0cd446c1ca48c2974e",
+    "url": "/RecipeMe/static/media/background.de7b0a6e.png"
+  }
+]);
