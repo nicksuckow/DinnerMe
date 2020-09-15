0bf148180b2397fa5c04f9eed182e1fa2692ccc6
Nick Suckow <suckow12@gmail.com>
2020-09-15 12:43:50 -0500

    adding side dish options

--- src/components/sidedishoptions.js
+++ src/components/sidedishoptions.js
@@ -16,6 +16,60 @@ const recipe=[
     },
     {
     "recipe": "Baked Beans"
+    },
+	{
+    "recipe": "Broccoli"
+    }, 
+    {
+    "recipe": "Asparagus"
+    }, 
+    {
+    "recipe": "Tomatoes"
+    }, 
+    {
+    "recipe": "Mashed Potatoes"
+    },
+    {
+    "recipe": "Baked Potatoes"
+    },
+	{
+    "recipe": "Cucumber Salad"
+    }, 
+    {
+    "recipe": "Carrots"
+    }, 
+    {
+    "recipe": "Brussel Sprouts"
+    }, 
+    {
+    "recipe": "Kiwi"
+    },
+    {
+    "recipe": "Papaya"
+    },
+	{
+    "recipe": "Cauliflower"
+    }, 
+    {
+    "recipe": "Coleslaw"
+    }, 
+    {
+    "recipe": "Dinner Rolls"
+    }, 
+    {
+    "recipe": "French Fries"
+    },
+    {
+    "recipe": "Macaroni Salad"
+    },
+	{
+    "recipe": "Mac n Cheese"
+    }, 
+    {
+    "recipe": "Pasta Salad"
+    }, 
+    {
+    "recipe": "Potato Salad"
     }
 ]
 
